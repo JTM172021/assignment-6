ceecffef4be1fe71c5d007526e2a5b9fdfeb14e3
JTM172021 <31741613+JTM172021@users.noreply.github.com>
2017-09-12 16:17:32 +0530

    Update ps2.py

--- ps2.py
+++ ps2.py
@@ -1,3 +1,22 @@
-###### this is the second .py file ###########
+###### this is the ps2 .py file ###########
 
-####### write your code here ##########
+#function to print tic tac toe board
+
+def print_board():
+	for i in range(0,3):
+		for j in range(0,3):
+			print map[2-i][j]
+			if j!=2:
+				print "|"
+		print ""
+
+def playgame():
+
+print "Welcome to the game"
+
+print "player 1 chance(choose numbers 1,3,5,7,9)"
+print "Enter the position and number to be entered:"
+pos=int(raw_input())
+num=int(raw_input())
+grid=[]
+gridresetgrid(grid)
