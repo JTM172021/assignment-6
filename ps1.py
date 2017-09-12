785714826fc13338a44ac72bf1ea638ff28f8808
JTM172021 <31741613+JTM172021@users.noreply.github.com>
2017-09-12 16:18:43 +0530

    Update ps1.py

--- ps1.py
+++ ps1.py
@@ -1,4 +1,5 @@
 ###### this is the ps1 .py file ###########
+
 n=raw_input()
 count=0
 count1=0
@@ -7,10 +8,14 @@ x=len(n)
 for i in range(0,x):
 	if n[i]=='1':
 		count=count+1
+
 #to concatinate 1 if even parity
+
 if count%2==0:
 	n_parity= n[:x]+'1'
+
 #concatinate 0 if odd parity
+
 else:
 	n_parity= n[:x]+'0'
 print n_parity
@@ -24,9 +29,9 @@ for i in range(0,x):
 	if n_parity[i:i+3]==str2:
 		index=i+3
 
-	n1_parity=n_parity[:index]+'0'+n_parity[index+1:]
+	n1_parity=n_parity[:index]+'0'+n_parity[index:]
 
-#print n1_parity
+print n1_parity
 
 #to add 0 if the frame ends with 01
 
@@ -41,4 +46,3 @@ if n_parity[x1-2:]==str1:
 modified_string=n1_parity[:len(n1_parity)]+'0101'
 
 print modified_string
-
